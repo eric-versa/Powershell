f36d3deab039909454e30dd3275b5411494dac2f
eric-versa <eric@versacomputing.com>
2015-07-08 11:39:10 -0700

    Update and rename adduser.ps1 to adduser-email365.ps1

---
+++ adduser-email365.ps1
@@ -0,0 +1,79 @@
+#This Script connects to office365 and creates a user from userinput "firstname" "lastname" 
+
+#Capture administrative credential for future connections.
+$credential = get-credential
+
+#Creates an Exchange Online session
+$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
+
+#Import session commands
+Import-PSSession $ExchangeSession 
+
+#This section creates a list of email domains to join the user to
+$x = @()
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+$objForm = New-Object System.Windows.Forms.Form 
+$objForm.Text = "Data Entry Form"
+$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm.StartPosition = "CenterScreen"
+
+$objForm.KeyPreview = $True
+
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
+    {
+        foreach ($objItem in $objListbox.SelectedItems)
+            {$x += $objItem}
+        $objForm.Close()
+    }
+    })
+
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
+    {$objForm.Close()}})
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+
+$OKButton.Add_Click(
+   {
+        foreach ($objItem in $objListbox.SelectedItems)
+            {$x += $objItem}
+        $objForm.Close()
+   })
+
+$objForm.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Size(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.Add_Click({$objForm.Close()})
+$objForm.Controls.Add($CancelButton)
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,20) 
+$objLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objLabel.Text = "Please make a selection from the list below:"
+$objForm.Controls.Add($objLabel) 
+
+$objListbox = New-Object System.Windows.Forms.Listbox 
+$objListbox.Location = New-Object System.Drawing.Size(10,40) 
+$objListbox.Size = New-Object System.Drawing.Size(260,20) 
+
+$objListbox.SelectionMode = "MultiExtended"
+
+[void] $objListbox.Items.Add("@s1lending.com")
+[void] $objListbox.Items.Add("@rfslends.com")
+
+$objListbox.Height = 70
+$objForm.Controls.Add($objListbox) 
+$objForm.Topmost = $True
+
+$objForm.Add_Shown({$objForm.Activate()})
+[void] $objForm.ShowDialog()
+
+$x
--- adduser.ps1
+++
@@ -1,9 +0,0 @@
-#Capture administrative credential for future connections.
-$credential = get-credential
-
-#Creates an Exchange Online session
-$ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $credential -Authentication Basic -AllowRedirection
-
-#Import session commands
-Import-PSSession $ExchangeSession 
-
